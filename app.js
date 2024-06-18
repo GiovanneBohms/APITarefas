90ff77dc32e37d1c05c1b3972f57cde72450c5c6
Giovanne <giovannebohms@gmail.com>
2024-06-18 13:23:41 -0300

    query de status e pesquisa por id

--- app.js
+++ app.js
@@ -12,24 +12,6 @@ const connection = mysql.createConnection({
     database: 'DesafioTecnico'
 });
 
-// Função para consultar todas as tarefas no banco de dados
-async function consultaTodasTarefas() {
-    return new Promise((resolve, reject) => {
-        const sql = `SELECT * FROM tarefa`;
-        connection.query(sql, (err, results) => {
-            if (err) {
-                console.error('Erro ao executar consulta:', err);
-                reject(err);
-            } else {
-                console.log('Todas as tarefas:');
-                console.log(results);
-                resolve(results);
-            }
-        });
-    });
-}
-
-
 async function consultaTarefaPorStatus(status){
 
     return new Promise ((resolve, reject)=>{
@@ -65,67 +47,6 @@ async function consultaTarefaPorId(id){
 }
 
 
-
-async function consultaTarefasDeUsuario(idUsuario) {
-    return new Promise((resolve, reject) => {
-        const sql = `SELECT * FROM tarefa WHERE usuario_id_usuario = ${idUsuario};`;
-        connection.query(sql, (err, results) => {
-            if (err) {
-                console.error('Erro ao executar consulta:', err);
-                reject(err);
-            } else {
-                console.log('Todas as tarefas de usuário');
-                console.log(results)
-                resolve(results);
-            }
-        });
-    });
-}
-
-async function consultaUsuarioPorId(idUsuario){
-    return new Promise((resolve, reject)=>{
-        const sql = `SELECT nome FROM usuario WHERE id_usuario = ${idUsuario};`
-        connection.query(sql, (err, results)=>{
-            if(err){
-                console.error('erro ao executar consulta de Nome de Usuário')
-            }else{
-                console.log('Nome de usuário');
-                console.log(results)
-                resolve(results);
-            }
-        })
-    })
-}
-
-async function inserirNovoUsuario (nome,senha){
-    if ((nome||senha) === undefined){
-        console.log('insira nome e senha corretamente')
-        return  
-    }
-    return new Promise ((resolve, reject)=>{
-        const sql=`INSERT INTO usuario (nome, senha) values ('${nome}', '${senha}');`
-        connection.query(sql, (err, results)=>{
-            if(err){
-                console.error('erro ao inserir novo usuário');
-            }else{
-                console.log('Novo usuário inserido com sucesso');
-                console.log(results);
-                resolve(results)
-            }
-        })
-    })
-}
-
-app.get('/api/tarefas', async (req, res) => {
-    try {
-        const tarefas = await consultaTodasTarefas();
-        res.status(200).json(tarefas)
-    } catch (err) {
-        console.error('Erro ao obter tarefas:', err);
-        res.status(500).send('Erro ao obter tarefas');
-    }
-});
-
 app.get('/api/tarefas/:id', async (req, res) => {
     const tarefaId = parseInt(req.params.id);
     try {
@@ -138,7 +59,7 @@ app.get('/api/tarefas/:id', async (req, res) => {
 });
 
 
-app.get('/api/tarefa', async (req, res) => {
+app.get('/api/tarefas', async (req, res) => {
     try {
         const termoDeBusca = req.query.q;
         const tarefaPorStatus = await consultaTarefaPorStatus(termoDeBusca);
@@ -149,26 +70,11 @@ app.get('/api/tarefa', async (req, res) => {
     }
 });
 
-// consultaTarefaPorStatus('ativo')
-
-app.get('/tarefas/usuario/:id', async (req, res) => {
-    const userId = parseInt(req.params.id);
-    try {
-        const tarefas = await consultaTarefasDeUsuario(userId);
-        const usuario = await consultaUsuarioPorId(userId)
-        res.status(200).json([usuario,tarefas]);
-    } catch (err) {
-        console.error('Erro ao obter tarefas do usuário:', err);
-        res.status(500).send('Erro ao obter tarefas do usuário');
-    }
-});
-
-
 
 // Inicia o servidor Express
 app.listen(port, () => {
     console.log(`Servidor online na porta ${port}`);
 });
 
-const urlRotas = ['http://localhost:3000/tarefas','ola']
+const urlRotas = ['http://localhost:3000/api/tarefas?q=pendente','http://localhost:3000/api/tarefas/2']
 console.table(urlRotas)
